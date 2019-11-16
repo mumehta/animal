e1d323b63595b8f9686db98e06af4a6e4a710ccb
Munish Mehta <munish.mehta@sbs.com.au>
2019-11-17 02:15:25 +1100

    First commit

---
+++ animal.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+import csv
+import json
+import pandas as pd
+
+try:
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Inputs client details', add_help=True, epilog='Example of use')
+    parser.add_argument('--name', type=str, help='client name', action='store', default=False, dest="name")
+    parser.add_argument('--address', type=str, help='client address', action='store', default=False, dest="address")
+    parser.add_argument('--phone', type=str, help='phone', action='store', default=False, dest="phone")
+    flags = parser.parse_args()
+
+except ImportError:
+    flags = None
+
+
+class SaveCSV:
+
+    def save_data(self, data):
+        csv_file = "client.csv"
+        csv_columns = ['name', 'address', 'phone']
+        try:
+            with open(csv_file, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                writer.writerow(data)
+        except IOError:
+            print("I/O error")
+
+
+class SaveJSON:
+
+    def save_data(self, data):
+        json_file = "client.json"
+        try:
+            with open(json_file, 'w') as jsonfile:
+                json.dump(data, jsonfile)
+        except IOError:
+            print("I/O error")
+
+
+SAVE_FORMATS = {
+    'csv': SaveCSV(),
+    'json': SaveJSON(),
+}
+
+
+def save_data(key, value):
+    SAVE_FORMATS[key].save_data(value)
+    SAVE_FORMATS[key].save_data(value)
+
+
+def supported_formats():
+    return list(SAVE_FORMATS.keys())
+
+
+def read_json_with_panda():
+    url = 'https://api.github.com/users/mumehta/repos'
+    # df = pd.read_json('client.json', orient='columns')
+    df = pd.read_json(url, orient='columns')
+    return df.head(10)
+
+
+if __name__ == '__main__':
+    print('Format(s) supported: ', supported_formats())
+    save_data('csv', flags.__dict__)
+    save_data('json', flags.__dict__)
+    print(read_json_with_panda())
+
